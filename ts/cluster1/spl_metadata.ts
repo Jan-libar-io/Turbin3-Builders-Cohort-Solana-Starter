d02bb1241e0fdce31d55387d87d3013638c40ffa
Jan J <jan.jankowski@libar.io>
2025-01-15 22:55:38 +0100

    spl metadata solution

--- ts/cluster1/spl_metadata.ts
+++ ts/cluster1/spl_metadata.ts
@@ -1,49 +1,64 @@
-import wallet from "../dev-wallet.json"
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { 
-    createMetadataAccountV3, 
-    CreateMetadataAccountV3InstructionAccounts, 
-    CreateMetadataAccountV3InstructionArgs,
-    DataV2Args
+import wallet from "../dev-wallet.json";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createMetadataAccountV3,
+  CreateMetadataAccountV3InstructionAccounts,
+  CreateMetadataAccountV3InstructionArgs,
+  DataV2Args,
 } from "@metaplex-foundation/mpl-token-metadata";
-import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-foundation/umi";
+import {
+  createSignerFromKeypair,
+  signerIdentity,
+  publicKey,
+} from "@metaplex-foundation/umi";
 import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
 
 // Define our Mint address
-const mint = publicKey("<mint address>")
+const mint = publicKey("8KgAkct5CLnPcNsY2kiU7egoVw8Hxh4iBRpsPcH53gJg");
 
 // Create a UMI connection
-const umi = createUmi('https://api.devnet.solana.com');
+const umi = createUmi("https://api.devnet.solana.com");
 const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
 umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
 
 (async () => {
-    try {
-        // Start here
-        // let accounts: CreateMetadataAccountV3InstructionAccounts = {
-        //     ???
-        // }
+  try {
+    let accounts: CreateMetadataAccountV3InstructionAccounts = {
+      mint: mint,
+      mintAuthority: signer,
+    };
 
-        // let data: DataV2Args = {
-        //     ???
-        // }
+    let data: DataV2Args = {
+      name: "Bober",
+      symbol: "BOB",
+      uri: "https://a.pinatafarm.com/300x252/43d02d069e/yelling-beaver.jpg",
+      sellerFeeBasisPoints: 10,
+      creators: [
+        {
+          address: keypair.publicKey,
+          verified: true,
+          share: 100,
+        },
+      ],
+      collection: null,
+      uses: null,
+    };
 
-        // let args: CreateMetadataAccountV3InstructionArgs = {
-        //     ???
-        // }
+    let args: CreateMetadataAccountV3InstructionArgs = {
+      data: data,
+      isMutable: false,
+      collectionDetails: null,
+    };
 
-        // let tx = createMetadataAccountV3(
-        //     umi,
-        //     {
-        //         ...accounts,
-        //         ...args
-        //     }
-        // )
+    let tx = createMetadataAccountV3(umi, {
+      ...accounts,
+      ...args,
+    });
 
-        // let result = await tx.sendAndConfirm(umi);
-        // console.log(bs58.encode(result.signature));
-    } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
-    }
+    let result = await tx.sendAndConfirm(umi);
+    console.log(bs58.encode(result.signature));
+  } catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+  }
 })();
